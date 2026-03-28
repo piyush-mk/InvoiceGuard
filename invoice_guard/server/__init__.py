ffd249c47024464778372551070ac7e6527ed267
Piyush Mohapatra <piyush.ofcl@gmail.com>
2026-03-28 11:30:52 +0530

    OpenEnv scaffoding and boilerplate.
    Remove requirements.txt file as part of project restructuring.

---
+++ invoice_guard/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Invoice Guard environment server components."""
+
+from .invoice_guard_environment import InvoiceGuardEnvironment
+
+__all__ = ["InvoiceGuardEnvironment"]
